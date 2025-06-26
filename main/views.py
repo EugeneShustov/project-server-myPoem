0bd0c0e7157f074e1786cb0f0c28af56fe4cea97
Eugene <prophecy601@gmail.com>
2025-06-26 18:10:17 +0300

    Обновлена работа и убран pycache

--- main/views.py
+++ main/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
 
-def poem_view(request):
-    return render(request, 'main/poem.html')
-
 def poem_test_view(request):
     return render(request, 'main/poem_test.html')
